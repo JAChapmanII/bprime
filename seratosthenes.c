71738d7d280fce01214db6ade61758f59bac5f93
Jeff Chapman II <jac@JAChapmanII.net>
2011-09-06 13:52:33 -0400

    Make window square, draw primes as pixels

--- seratosthenes.c
+++ seratosthenes.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <GL/gl.h>
 
-#define PRIME_COUNT 25
+#define PRIME_WIDTH 600
+#define PRIME_COUNT PRIME_WIDTH*PRIME_WIDTH
+
 char *prime = NULL;
 
 void generatePrimes() {
@@ -22,21 +25,31 @@ void generatePrimes() {
 	}
 }
 
-
-
 int main(int argc, char **argv) {
-	int i;
+	int i, j;
+	setPlotDimensions(PRIME_WIDTH, PRIME_WIDTH);
 	plot_CheckState();
 	clearPlot();
 
 	generatePrimes();
 
+	/*
 	for(i = 2; i < PRIME_COUNT; ++i)
 		if(prime[i])
 			printf("%d ", i);
 	printf("\n");
+	*/
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glPointSize(1);
+	glBegin(GL_POINTS);
+	for(i = 0; i < PRIME_WIDTH; ++i)
+		for(j = 0; j < PRIME_WIDTH; ++j)
+			if(prime[i*PRIME_WIDTH + j])
+				glVertex2f(i, j);
+	glEnd();
+	SDL_GL_SwapBuffers();
+
 	/*plotDelay(3000);*/
 }
-
-
 
