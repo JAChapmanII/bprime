1b6980de05b47612e632bf1b8f15158079e7cdc7
Jeff Chapman II <jac@JAChapmanII.net>
2011-10-14 13:09:54 -0400

    Use 4 threads, use faster bit counting algorithm for 64 bit numbers

--- bprime.c
+++ bprime.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <pthread.h>
 
 size_t PRIME_WIDTH, PRIME_HEIGHT, PRIME_COUNT, MEM_REQUIREMENT;
 
@@ -26,6 +28,19 @@ void setNotPrime(char *p, size_t x) {
 		p[x >> 3] -= (0x1 << (x % 8));
 } /* }}} */
 
+/* */
+#define m1  0x5555555555555555
+#define m2  0x3333333333333333
+#define m4  0x0f0f0f0f0f0f0f0f
+#define h01 0x0101010101010101
+
+size_t countSet(uint64_t x) {
+	x -= (x >> 1) & m1;
+	x = (x & m2) + ((x >> 2) & m2);
+	x = (x + (x >> 4)) & m4;
+	return (x * h01) >>56;
+}
+
 size_t generatePrimes();
 
 int main(int argc, char **argv) {
@@ -48,7 +63,7 @@ int main(int argc, char **argv) {
 	pCount = generatePrimes();
 	printf("Generated initial prime set...\n");
 	printf("%ld out of %ld numbers were prime\n", pCount, PRIME_COUNT);
-	for(i = width*height; i > 0; --i)
+	for(i = PRIME_COUNT; i > 0; --i)
 		if(isPrime(prime, i))
 			break;
 	printf("Biggest prime: %ld\n", i);
@@ -56,8 +71,13 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+void *crossOut(void *args);
+/* this is hard-coded in generatePrimes, anyway */
+size_t i = 0;
+#define THREAD_COUNT 4
 size_t generatePrimes() { /* {{{ */
-	size_t i, j, pCount = 0;
+	size_t pCount = 0, args[THREAD_COUNT << 1], m = sqrt(PRIME_COUNT) + 1;
+	pthread_t threads[THREAD_COUNT];
 	if(prime != NULL)
 		return -2;
 
@@ -68,13 +88,94 @@ size_t generatePrimes() { /* {{{ */
 	}
 	memset(prime, 0xff, MEM_REQUIREMENT);
 
-	for(i = 2; i < PRIME_COUNT; ++i) {
+	printf("PRIME_COUNT: %ld\n", PRIME_COUNT);
+
+	args[0] = 0;
+	args[4] = (PRIME_COUNT >> 4) << 3;
+
+	args[2] = (args[4] >> 4) << 3;
+	args[6] = args[4] + ((args[4] >> 4) << 3);
+
+	args[1] = args[2];
+	args[3] = args[4];
+	args[5] = args[6];
+	args[7] = PRIME_COUNT;
+
+	printf("t1: [%ld, %ld]\n", args[0], args[1]);
+	printf("t2: [%ld, %ld]\n", args[2], args[3]);
+	printf("t3: [%ld, %ld]\n", args[4], args[5]);
+	printf("t4: [%ld, %ld]\n", args[6], args[7]);
+	printf("m: %ld\n", m);
+
+	for(i = 2; i < m; ++i) {
+		if(i % 10000 == 0) {
+			printf("%ld...\n", i);
+			fflush(stdout);
+		}
 		if(!isPrime(prime, i))
 			continue;
 		pCount++;
-		for(j = i * 2; j < PRIME_COUNT; j += i)
-			setNotPrime(prime, j);
+
+		if(pthread_create(&threads[0], NULL, crossOut, (void *)(&args[0]))) {
+			fprintf(stderr, "thread creation 0 failed!\n");
+			exit(1);
+		}
+		if(pthread_create(&threads[1], NULL, crossOut, (void *)(&args[2]))) {
+			fprintf(stderr, "thread creation 1 failed!\n");
+			exit(1);
+		}
+		if(pthread_create(&threads[2], NULL, crossOut, (void *)(&args[4]))) {
+			fprintf(stderr, "thread creation 2 failed!\n");
+			exit(1);
+		}
+		if(pthread_create(&threads[3], NULL, crossOut, (void *)(&args[6]))) {
+			fprintf(stderr, "thread creation 3 failed!\n");
+			exit(1);
+		}
+
+		if(pthread_join(threads[0], NULL)) {
+			fprintf(stderr, "thread 0 join failed\n");
+			exit(1);
+		}
+		if(pthread_join(threads[1], NULL)) {
+			fprintf(stderr, "thread 1 join failed\n");
+			exit(1);
+		}
+		if(pthread_join(threads[2], NULL)) {
+			fprintf(stderr, "thread 2 join failed\n");
+			exit(1);
+		}
+		if(pthread_join(threads[3], NULL)) {
+			fprintf(stderr, "thread 3 join failed\n");
+			exit(1);
+		}
+		/* TODO: destroy threads? */
 	}
+	if(m % 64 != 0)
+		m = ((m + 64) >> 6) << 6;
+	for(; i < m; ++i)
+		if(isPrime(prime, i))
+			pCount++;
+	for(i >>= 6; i < PRIME_COUNT >> 6; ++i)
+		pCount += countSet(((uint64_t *)prime)[i]);
 	return pCount;
 } /* }}} */
+
+void *crossOut(void *args) {
+	size_t j;
+	if(((size_t *)args)[0] == 0) {
+		j = i * i;
+	} else {
+		j = (((size_t *)args)[0] / i) * i;
+		if(j < ((size_t *)args)[0])
+			j += i;
+	}
+	if(j <= i)
+		j += i;
+	if(((size_t *)args)[1] < j)
+		return NULL;
+	for(; j < ((size_t *)args)[1]; j += i)
+		setNotPrime(prime, j);
+	return NULL;
+}
 
